6c385e6b31a02e3dae79fda115243c0f5342a1eb
Evgeniy343 <ychuray@list.ru>
2022-02-13 23:15:17 +0300

    change some styles on pages, add a little bit documentation of AccountPageController class

--- src/main/java/by/evgen/Cafe/controller/AccountPageController.java
+++ src/main/java/by/evgen/Cafe/controller/AccountPageController.java
@@ -1,9 +1,7 @@
 package by.evgen.Cafe.controller;
 
 import by.evgen.Cafe.exception.MealNotFoundException;
-import by.evgen.Cafe.exception.UserNotFoundException;
 import by.evgen.Cafe.handler.CafeModelNotFoundExceptionHandler;
-import by.evgen.Cafe.model.impl.CafeUserModel;
 import by.evgen.Cafe.model.impl.MealCategory;
 import by.evgen.Cafe.model.impl.MealModel;
 import by.evgen.Cafe.service.CafeMealService;
@@ -16,12 +14,18 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+/**
+ * Класс-контроллер для работы с аккаунтом администратора
+ * @author Churai Evegniy
+ */
 @Controller
 @RequestMapping("/account")
 @CafeModelNotFoundExceptionHandler
 public class AccountPageController {
 
+    /** Поле сервиса для работы с блюдами */
     private final CafeMealService mealService;
+    /** Поле сервиса для работы с пользователями */
     private final CafeUserService userService;
 
     @Autowired
@@ -30,25 +34,24 @@ public class AccountPageController {
         this.userService = userService;
     }
 
-
-    @PostMapping("/authorization")
-    public String accountConfirmation(@RequestParam("login") String login, @RequestParam("password") String password,
-                                      Model model) throws UserNotFoundException {
-        CafeUserModel authorizedUser = userService.findByLoginAndPassword(login, password);
-        if(authorizedUser == null){
-            return "cafe/html/authentication/authorization";
-        }
-        model.addAttribute("authorizedUser",authorizedUser);
-        String accountUrl = "/account/" + authorizedUser.getId();
-        return "redirect:" + accountUrl;
-    }
-
-    @GetMapping("/{id}")
-    public String account(@PathVariable Long id,Model model) throws UserNotFoundException {
-        CafeUserModel authorizedUser = userService.findById(id);
-        model.addAttribute("authorizedUser", authorizedUser);
-        return "cafe/html/account/account";
-    }
+//    @PostMapping("/authorization")
+//    public String accountConfirmation(@RequestParam("login") String login, @RequestParam("password") String password,
+//                                      Model model) throws UserNotFoundException {
+//        CafeUserModel authorizedUser = userService.findByLoginAndPassword(login, password);
+//        if(authorizedUser == null){
+//            return "cafe/html/authentication/authorization";
+//        }
+//        model.addAttribute("authorizedUser",authorizedUser);
+//        String accountUrl = "/account/" + authorizedUser.getId();
+//        return "redirect:" + accountUrl;
+//    }
+//
+//    @GetMapping("/{id}")
+//    public String account(@PathVariable Long id,Model model) throws UserNotFoundException {
+//        CafeUserModel authorizedUser = userService.findById(id);
+//        model.addAttribute("authorizedUser", authorizedUser);
+//        return "cafe/html/account/account";
+//    }
 //    @GetMapping("/users")
 //    public String viewUsers(Model model) {
 //        model.addAttribute("users", userService.findAll());
@@ -61,18 +64,39 @@ public class AccountPageController {
 //        return "cafe_user/get/user";
 //    }
 
+
+    /**
+     * Функция get запроса, которая отображает категории блюд
+     * @param model - категории блюд
+     * @return возвращает название html файла, который отображает категории блюд
+     */
     @GetMapping("/menu")
     public String viewMenu(Model model) {
         model.addAttribute("categories", MealCategory.values());
         return "cafe/html/menu/categories";
     }
 
+    /**
+     * Функция get запроса, которая отображает блюда выбранной категории
+     * @param categoryName - название категории блюда
+     * @param model - все блюда выбранной категории
+     * @return возвращает название html файла, который отображает блюда выбранной категории
+     */
     @GetMapping("/menu/{category}/meals")
     public String viewMeals(@PathVariable("category") String categoryName, Model model) {
         model.addAttribute("meals", mealService.findAll());
         return "cafe/html/menu/meals";
     }
 
+    /**
+     * Функция get запроса, которая отображает блюдо выбранное пользователем конкретной категории
+     * @param categoryName - название категории блюда
+     * @param id - идентификатор блюда
+     * @param model - выбранное блюдо
+     * @return возвращает название html файла, который отображает блюдо выбранное пользователем данной категории
+     * @throws MealNotFoundException - исключение, выбрасывающееся в случаях, когда производится доступ к блюду,
+     * которго не сущетвует.
+     */
     @GetMapping("/menu/{category}/meals/{id}")
     public String viewMeal(@PathVariable("category") String categoryName,
                            @PathVariable("id") Long id, Model model) throws MealNotFoundException {
@@ -80,6 +104,11 @@ public class AccountPageController {
         return "cafe/html/menu/meal";
     }
 
+    /**
+     * Функция get запроса, которая отображает форму для создания нового блюда
+     * @param model - включает пустой объект блюда и категорию блюда
+     * @return возвращает название html файла, который отображает форму для создания нового блюда
+     */
     @GetMapping("/menu/create")
     public String createMealForm(Model model) {
         model.addAttribute("meal",new MealModel());
@@ -87,6 +116,13 @@ public class AccountPageController {
         return "cafe/html/menu/create-meal";
     }
 
+    /**
+     * Функция post запроса, которая создаёт блюдо и добавляет его в БД
+     * @param meal - блюдо, созданное из данных, введённых пользователем в форму для создания нового блюда
+     * @param bindingResult - проверка на корректность введённых данных при создании блюда
+     * @return возвращает строку, в которой происходит перенаправление на страницу категорий, а в случае неккоректно
+     * введённых данных возвращается название html файла, который отображает форму для создания нового блюда
+     */
     @PostMapping("/menu/create")
     public String createMeal(@ModelAttribute("meal") @Valid MealModel meal, BindingResult bindingResult) {
         if (bindingResult.hasErrors())
@@ -95,12 +131,28 @@ public class AccountPageController {
         return "redirect:/account/menu";
     }
 
+
+    /**
+     * Функция post запроса, которая удаляет блюдо из БД
+     * @param id - идентификатор блюда
+     * @param categoryName - название категории блюда
+     * @return возвращает строку, в которой происходит перенаправление на страницу блюд текущей категории
+     */
     @PostMapping("/menu/{category}/meals/{id}/delete")
     public String deleteMeal(@PathVariable("id") Long id, @PathVariable("category") String categoryName) {
         mealService.deleteById(id);
         return "redirect:/account/menu/{category}/meals";
     }
 
+    /**
+     * Функиция get запроса, которая отображает форму для редактирования блюда
+     * @param category - категория блюда
+     * @param id - идентификатор блюда
+     * @param model - блюдо, нуждающееся в редактировании
+     * @return возвращает название html файла, который отображает форму для редактирования блюда
+     * @throws MealNotFoundException исключение, выбрасывающееся в случаях, когда производится доступ к блюду,
+     * которго не сущетвует.
+     */
     @GetMapping("/menu/{category}/meals/{id}/edit")
     public String editMealForm(@PathVariable("category") MealCategory category,
                                @PathVariable("id") Long id, Model model) throws MealNotFoundException {
@@ -110,6 +162,18 @@ public class AccountPageController {
     }
 
 
+    /**
+     * Функция post запроса, которая редактирует блюдо и обновляет данные в БД
+     * @param meal - блюдо
+     * @param bindingResult - проверка на корректность введённых данных при редактировании блюда
+     * @param id - идентификатор блюда
+     * @param categoryName - название категории блюда
+     * @return возвращает строку, в которой происходит перенаправление на страницу с блюдами текущей категории,
+     * а в случае неккоректно введённых данных возвращается название html файла, который отображает форму для
+     * редактирования блюда
+     * @throws MealNotFoundException исключение, выбрасывающееся в случаях, когда производится доступ к блюду,
+     * которго не сущетвует.
+     */
     @PostMapping("/menu/{category}/meals/{id}/edit")
     public String editMeal(@ModelAttribute("meal") @Valid MealModel meal, BindingResult bindingResult,
                            @PathVariable("id") Long id,
--- src/main/java/by/evgen/Cafe/controller/MainPageController.java
+++ src/main/java/by/evgen/Cafe/controller/MainPageController.java
@@ -1,14 +1,26 @@
 package by.evgen.Cafe.controller;
 
 import by.evgen.Cafe.model.impl.CafeUserModel;
+import by.evgen.Cafe.model.impl.MealCategory;
+import by.evgen.Cafe.service.CafeMealService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/main_page")
 public class MainPageController {
+
+    private final CafeMealService mealService;
+
+    @Autowired
+    public MainPageController(CafeMealService mealService) {
+        this.mealService = mealService;
+    }
+
     @GetMapping("/authorization")
     public String authorizationForm(Model model){
         model.addAttribute("user",new CafeUserModel());
@@ -16,12 +28,19 @@ public class MainPageController {
     }
 
     @GetMapping()
-    public String mainPage(){
+    public String mainPage(Model model){
+        model.addAttribute("categories", MealCategory.values());
         return "cafe/html/main_page/main-page";
     }
 
     @GetMapping("/registration")
     public String registrationForm(){
         return "cafe/html/authentication/registration";
+    }
+
+    @GetMapping("/{category}/meals")
+    public String viewMeals(@PathVariable("category") String categoryName, Model model) {
+        model.addAttribute("meals", mealService.findAll());
+        return "cafe/html/menu/meals";
     }
 }
